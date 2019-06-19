57ec525d87effdb0085d1a98a2303ca2cfc9bbc9
jackBonadies <jbonadies6@gmail.com>
2019-06-19 17:44:50 -0400

    mac build

---
+++ release-builds/slopify-darwin-x64/slopify.app/Contents/Resources/app/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ef4f1955e90dab4140791dabad293834b81b63f017da199c824413a24a6d5be
+size 220
---
+++ release-builds/slopify-darwin-x64/slopify.app/Contents/Resources/app/helloworld.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c94bb0bdf665bcaeab6644f09d8c1ac0ee45660c2cd083c465779ff9db66c303
+size 4337
